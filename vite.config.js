51a45ee8e6154ab30550a3b899e275e77f9dda69
htsnowday <127791461+htsnowday@users.noreply.github.com>
2025-04-23 15:34:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // Ensure it's using the root folder
+  publicDir: 'public',
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
+    rollupOptions: {
+      input: './public/index.html' // This tells Vite where to start
+    }
+  }
 });
